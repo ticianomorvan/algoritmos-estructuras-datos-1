49ed3c7e7ed13843df93ec045fd77c7438ffa32a
Ticiano Morvan <ticiano.morvan@gmakl.com>
2023-09-25 21:37:41 +0000

    update: add conditional execution of exercises

--- Proyecto3.c
+++ Proyecto3.c
@@ -35,16 +35,31 @@
   Luego, como en C tenemos que 1 es equivalente a true y 0 equivalente a false, los valores sugeridos cumplen con las tres expresiones.
 */
 
+int obtenerEjercicio () {
+  int e;
+
+  printf("Ingrese el ejercicio a ejecutar (ej: 1): ");
+  scanf("%i", &e);
+ 
+  return e;
+}
+
+int pedirEntero (char v) {
+  int n;
+  
+  printf("Ingrese un valor entero para %c: ", v);
+  scanf("%i", &n);
+
+  return n;
+}
+
 void ejercicioUno () {
   int x, y, z;
-  
-  printf("Ingrese un valor entero para x: ");
-  scanf("%i", &x);
-  printf("Ingrese un valor entero para y: ");
-  scanf("%i", &y);
-  printf("Ingrese un valor entero para z: ");
-  scanf("%i", &z);
 
+  x = pedirEntero('x');
+  y = pedirEntero('y');
+  z = pedirEntero('z');
+  
   printf("Ejercicio 1\n");
   printf("x + y + 1 = %d\n", x + y + 1 );
   printf("z * z + y * 45 - 15 * x = %d\n", z * z + y * 45 - 15 * x);
@@ -54,21 +69,33 @@ void ejercicioUno () {
 }
 
 void ejercicioDos () {
-  int b, w;
+  int x, y, z, b, w;
 
-  printf("Ingrese un valor entero para b: ");
-  scanf("%i", &b);
-  printf("Ingrese un valor entero para w: ");
-  scanf("%i", &w);
-
+  x = pedirEntero('x');
+  y = pedirEntero('y');
+  z = pedirEntero('z');
+  b = pedirEntero('b');
+  w = pedirEntero('w');
+  
   printf("Ejercicio 2\n");
   printf("x %% 4 == 0; si x = %d, el resultado es %d\n", x, x % 4 == 0);
   printf("x + y == 0 && y - x == (-1) * z; si x = %d, y = %d, z = %d, el resultado es %d\n", x, y, z, x + y == 0 && y - x == (-1) * z);
   printf("not b && w; si b = %d, w = %d, el resultado es %d\n", b, w, !b && w);
 }
 
-int main(int argc) {
-  printf("%d", argc)
+int main() {
+  int ejercicio = obtenerEjercicio();
+
+  switch (ejercicio) {
+    case 1:
+      ejercicioUno();
+      break;
+    case 2:
+      ejercicioDos();
+      break;
+    default:
+      break;
+  }
 
   return 0;
 }
